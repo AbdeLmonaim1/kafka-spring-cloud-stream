737878e015d8601a7c4581189bfa23cc10401f11
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-10-07 15:18:45 +0100

    feat: add real-time page analytics with Kafka Streams and Smoothie.js visualization
    - Implemented a `Supplier<PageEvent>` bean to generate random page events.
    - Added a `Function<KStream<String, PageEvent>, KStream<String, Long>>` to filter, group, and count events within 5-second windows using Kafka Streams.
    - Integrated `/analytics` endpoint using Flux and InteractiveQueryService to stream live analytics data.
    - Created `index.html` with Smoothie.js to visualize page event counts in real time.

--- src/main/java/ma/sdia/kafkaspringcloudstream/handlers/PageEventHandler.java
+++ src/main/java/ma/sdia/kafkaspringcloudstream/handlers/PageEventHandler.java
@@ -1,10 +1,15 @@
 package ma.sdia.kafkaspringcloudstream.handlers;
 
 import ma.sdia.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
+import java.time.Duration;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 @Component
 public class PageEventHandler {
@@ -16,4 +21,28 @@ public class PageEventHandler {
             System.out.println("**************************");
         };
     }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2",
+                    Math.random()>0.5?"Monaim":"Amine",
+                    new java.util.Date(),
+                    (long) (10+new java.util.Random().nextInt(10000))
+            );
+        };
+    }
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction(){
+        return (input) ->
+                input.filter((k,v)->v.duration()>100)
+                        .map((k,v)-> new KeyValue<>(v.name(),v.duration()))
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                        .count(Materialized.as("count-store"))
+                        .toStream()
+                        .map((k,v)-> new KeyValue<>(k.key(),v))
+                ;
+    }
+
 }
